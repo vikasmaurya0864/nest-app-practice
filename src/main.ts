130b8755e553f37fe5c2a08bec822935dc4057f0
vikas.maurya <vikas.maurya@us.inc>
2026-06-28 13:15:03 +0530

    updated main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('NestJs API Routes')
+    .setDescription('API documentation for my NestJS app')
+    .setVersion('1.0')
+    .addBearerAuth() // If you use JWT auth
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
